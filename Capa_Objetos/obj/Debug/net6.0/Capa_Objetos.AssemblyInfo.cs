3f99c4f1ccd96d75f695cfd85263bd065c129f82
emendez26 <146005168+emendez26@users.noreply.github.com>
2023-11-06 20:56:02 -0500

    Resolucion de Conflictos

--- Capa_Objetos/obj/Debug/net6.0/Capa_Objetos.AssemblyInfo.cs
+++ Capa_Objetos/obj/Debug/net6.0/Capa_Objetos.AssemblyInfo.cs
@@ -11,13 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Capa_Objetos")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Capa_Objetos")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Capa_Objetos")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+
 
 // Generado por la clase WriteCodeFragment de MSBuild.
 
