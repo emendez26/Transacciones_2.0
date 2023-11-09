58b06ead076d0745a4854dfcb3785bef881c4e5b
emendez26 <146005168+emendez26@users.noreply.github.com>
2023-11-09 10:45:37 -0500

    ok

--- Capa_Datos/Conexion.cs
+++ Capa_Datos/Conexion.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
 
 namespace Capa_Datos
 {
     internal class Conexion
     {
+        private SqlConnection conexion;// = new SqlConnection(con);
+
+        public void iniciarBD(int sw_bd)
+        {
+            string BaseDatos = "";
+            if (sw_bd == 1)
+            {
+                BaseDatos = "Epicor10Live";
+            }
+            else
+            {
+                BaseDatos = "DB_TECNOFUEGO";
+            }
+
+            string con = "Server=10.25.5.15;DataBase=" + BaseDatos + "; user=sa; password=TecFire8901%; Integrated Security=false";
+            SqlConnection conexion = new SqlConnection(con);
+        }
+
+        public SqlConnection AbrirConexion()
+        {
+            if (conexion.State == ConnectionState.Closed)
+                conexion.Open();
+            return conexion;
+        }
+
+        public SqlConnection CerrarConexion()
+        {
+            if (conexion.State == ConnectionState.Open)
+                conexion.Close();
+            return conexion;
+        }
     }
 }
